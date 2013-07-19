fb333144b56745051600b17f8d9435307447d8d2
BouchkatiTarek <tarekbouchkati@gmail.com>
2013-07-19 08:49:29 -0700

    Update example.c

--- example.c
+++ example.c
@@ -1,1 +1,12 @@
-
+/*
+ * ChibiOS/DCMotor is a small library to easily interface L293D
+ * in order control DC Motors with ChibiOS/RT.
+ *
+ * This library is compatible STM32
+ *
+ * BSD licensed
+ *
+ * Author: Tarek Bouchkati   <tarekbouchkati@gmail.com>
+ */
+ 
+ 
